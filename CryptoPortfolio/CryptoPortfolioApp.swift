ca7b4f833a570bc909fcaeb0d946e96f190a017a
Damian <damianrydzewski.dev@gmail.com>
2023-02-18 11:52:47 +0100

    Initial Commit

---
+++ CryptoPortfolio/CryptoPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioApp.swift
+//  CryptoPortfolio
+//
+//  Created by Damian on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
